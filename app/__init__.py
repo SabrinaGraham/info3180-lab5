7872b23576c4822bf573b33e8efcbb27600fbe54
Sabrina Graham <techiepotato3004@gmail.com>
2022-03-04 21:16:05 -0500

    first migration

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Flask-Login login manager
 login_manager = LoginManager()
